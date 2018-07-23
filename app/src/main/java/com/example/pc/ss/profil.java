79d2075369dec9956e258a8f7f698d1742ac3b30
Ivan Ivanov <41294423+ivanovvbg@users.noreply.github.com>
2018-07-23 06:26:25 +0300

    added tree activity

--- app/src/main/java/com/example/pc/ss/profil.java
+++ app/src/main/java/com/example/pc/ss/profil.java
@@ -1,13 +1,31 @@
 package com.example.pc.ss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class profil extends AppCompatActivity {
-
+Button button5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        button5 = findViewById(R.id.button);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(profil.this, tree.class));
+
+            }
+        });
+
+
     }
 }
+
+
+
+
---
+++ app/src/main/java/com/example/pc/ss/tree.java
@@ -0,0 +1,13 @@
+package com.example.pc.ss;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tree);
+    }
+}
