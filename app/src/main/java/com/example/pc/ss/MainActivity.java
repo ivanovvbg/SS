6edc4341d680f85013d5e761beba7bc4adb5ae26
Ivan Ivanov <41294423+ivanovvbg@users.noreply.github.com>
2018-07-25 10:30:03 +0300

    idk

--- app/src/main/java/com/example/pc/ss/MainActivity.java
+++ app/src/main/java/com/example/pc/ss/MainActivity.java
@@ -5,13 +5,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     Button button6;
+    Button button7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, login1.class));
+            }
+        });
+        button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OptionMenu.class));
             }
         });
 
---
+++ app/src/main/java/com/example/pc/ss/OptionMenu.java
@@ -0,0 +1,22 @@
+package com.example.pc.ss;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class OptionMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
